8494aee0deed64cf1e1ab9ce3508297686d831a7
RedMX <50285835+RedMX@users.noreply.github.com>
2022-12-16 21:30:02 +0100

    Update eye-click.py

--- eye-click.py
+++ eye-click.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import pyautogui
 blnk=False
+hold=False # toggles the click or hold mode
 # adjust the blink threshold for both eyes for me 0.006 is good
 # the program checks for values below the threshold because as you close your eyes the distance between the top and bottom of the eye decreases
 right_eye_thresh=0.006
@@ -14,6 +15,7 @@ drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture('/dev/video1') # select your video device (here it is the second video device on linux)
 # for windows it would probably be like: cap = cv2.VideoCapture(0) for your first video device
 def chck_click(faces,rt_thresh,lt_thresh):
+  global hold
   global blnk
   for i in range(len(faces)):
     face=faces[i].landmark
@@ -21,10 +23,15 @@ def chck_click(faces,rt_thresh,lt_thresh):
     #print(face[374].y-face[386].y) print distance beetwen the top and the bottom of the left eye
     if face[145].y-face[159].y<rt_thresh and face[374].y-face[386].y<lt_thresh:
         if blnk==False:
-          pyautogui.click()
+          if hold==False:
+            pyautogui.click()
+          else:
+            pyautogui.mouseDown()
           blnk=True
     else:
-          blnk=False
+      if hold==True and blnk==True:
+        pyautogui.mouseUp()
+      blnk=False
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,# adjust it to your liking
     refine_landmarks=True,
